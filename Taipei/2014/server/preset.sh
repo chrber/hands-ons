3c841348ec93bf3a7f9d56df977770209bd47f77
Christian Bernardt <christian.bernardt@desy.de>
2014-02-21 14:57:01 +0000

    Preset does not configure postgres anymore, postgres.sh script does that.

--- Taipei/2014/server/preset.sh
+++ Taipei/2014/server/preset.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 rpm -Uhv http://yum.postgresql.org/9.2/redhat/rhel-6-x86_64/pgdg-sl92-9.2-8.noarch.rpm
-wget http://www.dcache.org/downloads/1.9/repo/2.6/dcache-2.6.21-1.noarch.rpm -O dcache-2.6.21-1.noarch.rpm 
+wget http://www.dcache.org/downloads/1.9/repo/2.6/dcache-2.6.21-1.noarch.rpm -O ~/dcache-2.6.21-1.noarch.rpm 
 
 yum install postgresql92-server.x86_64 -y
 yum install java-1.7.0-openjdk.x86_64 -y
@@ -13,33 +13,6 @@ service rpcbind restart
 echo "alias nfs-layouttype4-1 nfs_layout_nfsv41_files" > /etc/modprobe.d/dist-nfsv41.conf
 
 modprobe nfs_layout_nfsv41_files
-
-echo "#/bin/bash
-service postgresql-9.2 initdb
-sed -ie 's/max_connections = 100/max_connections = 1000/' /var/lib/pgsql/9.2/data/postgresql.conf
-service postgresql-9.2 start
-chkconfig postgresql-9.2 on
-
-mv /var/lib/pgsql/9.2/data/pg_hba.conf{,_ori}
-cat >/var/lib/pgsql/9.2/data/pg_hba.conf <<EOF
-# TYPE DATABASE USER CIDR-ADDRESS METHOD
-
-# \"local\" is for Unix domain socket connections only
-local all all trust
-# IPv4 local connections:
-host all all 127.0.0.1/32 trust
-# IPv6 local connections:
-host all all ::1/128 trust
-EOF
-service postgresql-9.2 restart
-createdb -U postgres chimera
-createuser -U postgres --no-superuser --no-createrole --createdb chimera
-psql -U chimera chimera -f /usr/share/dcache/chimera/sql/create.sql
-createlang -U postgres plpgsql chimera
-psql -U chimera chimera -f /usr/share/dcache/chimera/sql/pgsql-procedures.sql
-createuser -U postgres --no-superuser --no-createrole --createdb srmdcache
-createdb -U srmdcache dcache" > configurepgsql.sh;
-chmod +x configurepgsql.sh;
 
 echo "for index in \`seq 1 100\`;
 do
@@ -81,10 +54,6 @@ while [[ \$counter -le \$no_of_files ]]
   done" > gen_rand_files.sh
 
 chmod +x gen_rand_files.sh
-
-wget http://svn.dcache.org/downloads/hands-on/ca_dCacheORG-2.1-1.noarch.rpm -O ca_dCacheORG-2.1-1.noarch.rpm
-
-yum localinstall ca_dCacheORG-2.1-1.noarch.rpm -y
 
 iptables -F
 sed -ie 's/#Domain = local.domain.edu/Domain = taipei-domain/' /etc/idmapd.conf
