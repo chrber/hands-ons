5490ad9a5a61b72cf0c5f6b870fca95b35ceaad0
Christian Bernardt <christian.bernardt@desy.de>
2014-02-25 13:12:44 +0000

    client: create dcacheuser user, create directory for user key and cert. Copy hostcert for use as userkey and cert.

--- Taipei/2014/client/preset.sh
+++ Taipei/2014/client/preset.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+useradd dcacheuser
+
+if [ ! -d /home/dcacheuser/.globus ]; 
+then 
+    mkdir /home/dcacheuser/.globus; 
+else 
+    echo "/home/dcacheuser/.globus already exists"; 
+fi
+
+mv /etc/grid-security/hostcert.pem /home/dcacheuser/usercert.pem
+mv /etc/grid-security/hostkey.pem /home/dcacheuser/userkey.pem
+
 wget http://repository.egi.eu/sw/production/cas/1/current/repo-files/EGI-trustanchors.repo -O /etc/yum.repos.d/EGI-trust.repo
 yum install ca-policy-egi-core -y
 
