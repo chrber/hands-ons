d2db4bbbd3b88c0ddf2f0310dfe9ac6306199af4
TsiO <oleg@gpysik.rwth-aachen.de>
2014-03-06 09:20:36 +0000

    Add convertation of the hostkey from PKCS#8 to PKCS#1 format

--- Taipei/2014/server/preset.sh
+++ Taipei/2014/server/preset.sh
@@ -62,6 +62,10 @@ while [[ \$counter -le \$no_of_files ]]
 
 chmod +x gen_rand_files.sh
 
+mv /etc/grid-security/hostkey.pem{,-pk8}
+openssl rsa -in /etc/grid-security/hostkey.pem-pk8 -out /etc/grid-security/hostkey.pem
+
+
 iptables -F
 cp /etc/idmapd.conf /etc/idmapd.conf.bak
 sed -ie 's/#Domain = local.domain.edu/Domain = taipei-domain/' /etc/idmapd.conf
