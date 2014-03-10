629868689a3751db2607b05a5612d40d55adcaae
Christian Bernardt <christian.bernardt@desy.de>
2014-03-10 14:25:11 +0100

    server: presets-Remove generating random big files

--- Taipei/2014/server/preset.sh
+++ Taipei/2014/server/preset.sh
@@ -24,39 +24,6 @@ echo "alias nfs-layouttype4-1 nfs_layout_nfsv41_files" > /etc/modprobe.d/dist-nf
 
 modprobe nfs_layout_nfsv41_files
 
-echo "#/bin/bash
-no_of_files=120
-counter=101
-while [[ $counter -le \$no_of_files ]]
-  do
-   dd bs=256 count=\$RANDOM if=/dev/urandom of=random-file.\$counter
-   cp   random-file.\$counter /nfs4/random-file.\$counter
-   rm  -f random-file.\$counter
-   let \"counter += 1\"
-  done
-
-no_of_files=225
-counter=201
-while [[ \$counter -le \$no_of_files ]]
-  do
-   dd bs=256 count=\$((RANDOM%200+1)) if=/dev/urandom of=random-file.\$counter
-   cp   random-file.\$counter /nfs4/random-file.\$counter
-   rm  -f random-file.\$counter
-   let \"counter += 1\"
-  done
-
-no_of_files=325
-counter=301
-while [[ \$counter -le \$no_of_files ]]
-  do
-   dd bs=256 count=\$((RANDOM%20+1)) if=/dev/urandom of=random-file.\$counter
-   cp   random-file.\$counter /nfs4/random-file.\$counter
-   rm  -f random-file.\$counter
-   let \"counter += 1\"
-  done" > gen_rand_files.sh
-
-chmod +x gen_rand_files.sh
-
 mv /etc/grid-security/hostkey.pem{,-pk8}
 openssl rsa -in /etc/grid-security/hostkey.pem-pk8 -out /etc/grid-security/hostkey.pem
 
