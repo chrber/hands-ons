a1782e8c0cde7d8bb674f5fbfd206193ed11fb06
lumaz <lumaz.rzg.mpg.de>
2014-08-20 15:05:55 +0200

    Modified setupPuppetClient.sh for installation on gks-111 and gks-112

--- Karlsruhe/2014/puppet/client/setupPuppetClient.sh
+++ Karlsruhe/2014/puppet/client/setupPuppetClient.sh
@@ -7,6 +7,31 @@
 
 PUPPETCONF=/etc/puppet/puppet.conf
 PUPPETMASTER="gks-121.scc.kit.edu"
+PUPPETSSLDIR=/var/lib/puppet/ssl
+
+service puppet stop
+
+
+if [ -f $PUPPETCONF ]; then
+
+  mv $PUPPETCONF "$PUPPETCONF".bkp
+  echo "[INFO] $PUPPETCONF.bkp created"
+
+else 
+
+  echo "[INFO] $PUPPETCONF there isn't"
+
+fi
+
+if [ -d  ]; then
+  # Control will enter here if $DIRECTORY exists.
+  rm -fr /var/lib/puppet/ssl/*
+  echo "[INFO] $PUPPETSSLDIR deleted"
+else 
+
+  echo "[INFO] $PUPPETSSLDIR not present"
+  
+fi
 
 #Install puppet repo
 
@@ -19,8 +44,11 @@ yum install -y puppet
 # configure client for master connection.
 if [ -e $PUPPETCONF ] 
   then
-    echo "server = $PUPPETMASTER" >> $PUPPETCONF
+    echo "    server = $PUPPETMASTER" >> $PUPPETCONF
   else 
     echo "[ERROR] PUPPETCONF variable missing"
     exit 1 
 fi
+
+service puppet stop
+chkconfig puppet off
